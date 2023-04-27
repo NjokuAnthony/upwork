4963728b9ff55d616a69c1d029b26b62ed2fe4e5
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-04-27 23:47:44 +0100

    changed color

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -66,7 +66,7 @@ const Form = () => {
     </div>
   </div>
   <div class="col-12">
-    <button class="btn btn-primary" type="submit">Submit form</button>
+    <button class="btn btn-danger" type="submit">Submit form</button>
   </div>
 </form>
     </div>
